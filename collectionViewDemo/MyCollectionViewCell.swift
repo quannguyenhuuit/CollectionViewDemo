971f1ca41e736a18d68704284de734af078aedaa
quanmacos <quanmacos@quanmacoss-Mac.local>
2017-04-29 17:33:53 +0700

    CollectionViewDemo

---
+++ collectionViewDemo/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  collectionViewDemo
+//
+//  Created by quanmacos on 4/29/17.
+//  Copyright © 2017 quanmacos. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myImageView: UIImageView!
+}
